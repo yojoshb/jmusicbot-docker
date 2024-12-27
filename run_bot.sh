1439f15f804e2c2f1ed9312f2209b0b4099d3933
Teknicallity <joshua.s.3322@gmail.com>
2024-12-26 23:18:37 -0500

    Allow version tag to be specified

--- run_bot.sh
+++ run_bot.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-RELEASE_JSON=$(curl --silent "https://api.github.com/repos/${BOT_GITHUB}/releases/latest")
+# Get latest or specified tag version
+if [ "$BOT_VERSION" == "latest" ]; then
+  RELEASE_JSON=$(curl --silent "https://api.github.com/repos/${BOT_GITHUB}/releases/latest")
+else
+  RELEASE_JSON=$(curl --silent "https://api.github.com/repos/${BOT_GITHUB}/releases/tags/${BOT_VERSION}")
+fi
 
-VER_DEFAULT=$(echo "$RELEASE_JSON" | grep -Po '"tag_name":\s*"\K[^"]*')
+# Check if the release JSON was fetched successfully
+if [ -z "$RELEASE_JSON" ] || echo "$RELEASE_JSON" | grep -q '"message": "Not Found"'; then
+  echo "Error: Could not find release for version '$BOT_VERSION'."
+  exit 1
+fi
+
+VERSION_TAG=$(echo "$RELEASE_JSON" | grep -Po '"tag_name":\s*"\K[^"]*')
 ASSET_NAME=$(echo "$RELEASE_JSON" | grep -Po '"name":\s*"\KJMusicBot[^"]*\.jar')
 DOWNLOAD_URL=$(echo "$RELEASE_JSON" | grep -Po '"browser_download_url":\s*"\Khttps://[^"]*JMusicBot[^"]*\.jar')
 
-# If the ENV is not explicitly set, use the sourced ENV from the Dockerfile
-if [ "$BOT_VERSION" == "latest" ]; then
-  BOT_VERSION=$VER_DEFAULT
-fi
-
-echo -e "Downloading JMusicBot $BOT_VERSION"
+echo -e "Downloading JMusicBot $VERSION_TAG"
 if [ ! -f "$ASSET_NAME" ]; then
-  wget "$DOWNLOAD_URL"
+  wget "$DOWNLOAD_URL" -O "$ASSET_NAME"
 fi
 
-echo -e "Starting JMusicBot $BOT_VERSION"
+echo -e "Starting JMusicBot $VERSION_TAG"
 java -Dnogui=true -Dconfig=/config/config.txt -jar "$ASSET_NAME"
